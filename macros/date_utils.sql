5ade86ee3e45ce85b23d594b74f0535cad7e6ffe
yongligee@gmail.com <yongligee@gmail.com>
2026-06-11 15:01:06 +0000

    daily_weather model created

---
+++ macros/date_utils.sql
@@ -0,0 +1,16 @@
+{% macro get_season(x) %}
+CASE 
+WHEN MONTH(TO_TIMESTAMP({{x}})) IN (12,1,2) THEN 'WINTER'
+WHEN MONTH(TO_TIMESTAMP({{x}})) IN (3,4,5) THEN 'SPRING'
+WHEN MONTH(TO_TIMESTAMP({{x}})) IN (6,7,8) THEN 'SUMMER'
+ELSE 'AUTUMN'
+END
+{% endmacro %}
+
+
+{% macro day_type(x) %}
+CASE 
+WHEN DAYNAME(TO_TIMESTAMP({{x}})) IN ('Sat','Sun') THEN 'WEEKEND'
+ELSE 'BUSINESSDAY'
+END
+{% endmacro %}
